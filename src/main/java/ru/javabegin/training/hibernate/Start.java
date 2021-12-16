7bbb7380b265578ebb6745f7aa9bd05b0a44e3d3
alezalez8 <5446943@ukr.net>
2021-12-16 22:01:46 +0200

    end of lesson 6

---
+++ src/main/java/ru/javabegin/training/hibernate/Start.java
@@ -0,0 +1,12 @@
+package ru.javabegin.training.hibernate;
+
+import org.hibernate.Session;
+
+public class Start {
+    public static void main(String[] args) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+
+
+    }
+}
